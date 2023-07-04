63b927ef318bba656ddfa8a64de7be631c03c5af
muhamaz <humamzaky7@gmail.com>
2023-07-04 14:32:02 +0700

    verify test on cart page

--- src/main/java/com/saucedemo/pages/CartPage.java
+++ src/main/java/com/saucedemo/pages/CartPage.java
@@ -1,4 +1,51 @@
 package com.saucedemo.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage extends BasePage{
+    private By verifyCart = By.xpath("//div[@id='cart_contents_container']");
+    private By verifyCartItem = By.xpath("//div[@class='cart_item']");
+    private By checkoutBtn = By.xpath("//button[@id='checkout']");
+    private By contShopBtn = By.xpath("//button[@id='continue-shopping']");
+
+
+    public boolean verifyContentCartPage(){
+        return driver.findElement(verifyCart).isDisplayed();
+    }
+
+    public boolean verifyCartItem(){
+        return driver.findElement(verifyCartItem).isDisplayed();
+    }
+
+    public boolean verifyCheckoutBtn(){
+        return driver.findElement(checkoutBtn).isEnabled();
+    }
+
+    public boolean verifyContinueShopBtn(){
+        return driver.findElement(contShopBtn).isEnabled();
+    }
+
+    public void clickContinueShopBtn(){
+        clickBtn(contShopBtn);
+    }
+
+    public void clickCheckoutBtn(){
+        clickBtn(checkoutBtn);
+    }
+
+    /**
+     * Getting product name on cart page
+     * @param productName is name of product
+     */
+    public String getProductName(String productName){
+        return getText(By.xpath("//div[.='"+productName+"']"));
+    }
+
+    /**
+     * Getting product price on cart page
+     * @param index start at 3
+     */
+    public String getProductPrice(String index){
+        return getText(By.xpath("div:nth-of-type("+index+") .inventory_item_price"));
+    }
 }
--- src/test/java/com/saucedemo/tests/CartPageTest.java
+++ src/test/java/com/saucedemo/tests/CartPageTest.java
@@ -1,4 +1,31 @@
 package com.saucedemo.tests;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
 public class CartPageTest extends BaseTest{
+
+    @Test
+    public void verifyCartpage(){
+        loginPage.login("standard_user", "secret_sauce");
+        homePage.clickCartIcon();
+        assertTrue(cartPage.verifyContentCartPage());
+    }
+
+    @Test
+    public void verifyContinueBtn(){
+        loginPage.login("standard_user", "secret_sauce");
+        homePage.clickCartIcon();
+        assertTrue(cartPage.verifyContinueShopBtn());
+        cartPage.clickContinueShopBtn();
+        assertTrue(homePage.verifyHomePage());
+    }
+
+    @Test
+    public void verifyCheckoutBtn(){
+        loginPage.login("standard_user", "secret_sauce");
+        homePage.clickCartIcon();
+        assertTrue(cartPage.verifyCheckoutBtn());
+        cartPage.clickCheckoutBtn();
+    }
 }
