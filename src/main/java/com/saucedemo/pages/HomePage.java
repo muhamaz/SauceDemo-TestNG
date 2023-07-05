b7bb2b29e30a33768a34dfa7b8394f86f53180da
muhamaz <humamzaky7@gmail.com>
2023-07-05 15:17:59 +0700

    add comment

--- src/main/java/com/saucedemo/pages/HomePage.java
+++ src/main/java/com/saucedemo/pages/HomePage.java
@@ -22,10 +22,17 @@ public class HomePage extends BasePage{
         filterProduct.selectByVisibleText(filterBy);
         return this;
     }
+
+    /**
+     * @param productName example : Sauce Labs Onesie
+     */
     public String verifyFilterName(String productName){
         return getText(By.xpath("//div[.='"+productName+"']"));
     }
 
+    /**
+     * @param index start at 1
+     */
     public String verifyFilterPrice(String index){
         return getText(By.cssSelector(".inventory_list > div:nth-of-type("+index+") .inventory_item_price"));
     }
@@ -49,6 +56,9 @@ public class HomePage extends BasePage{
         clickBtn(logoutBtn);
     }
 
+    /**
+     * @param productName example : sauce-labs-backpack
+     */
     public HomePage addProduct(String productName){
         driver.findElement(By.xpath("//button[contains(@id,'"+productName+"')]")).click();
         return this;
